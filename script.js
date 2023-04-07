2d156c10a8b25db8020a677faf4914cb51bf9fb3
rushi-reactdev <rushikeshkale669@gmail.com>
2023-04-07 23:15:59 +0530

    learn the array method in javascript and done the operation on array

---
+++ script.js
@@ -0,0 +1,26 @@
+// here is the concept and examples of array i learn today
+//how to replace elements in array
+let sports = ["cricket","foortball","voleyball","kabaddi"];
+sports[1]= "swimming";
+console.log(sports);
+// how to add the element in last index in array
+sports.push("hide and seek");
+console.log(sports);
+// how to remove the element from last index and create new variable by that element
+sports.pop();
+console.log(sports);
+// how to add the element at start in array
+sports.unshift("badminton");
+console.log(sports);
+// how  to seprate the element from array
+sports.splice(3,1);
+console.log(sports);
+//  how to delete an element from starting of array
+let favouraite =  sports.shift();
+console.log(sports);
+console.log(favouraite);
+
+
+
+
+
